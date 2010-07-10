4abc764e11e2c23a569664293aa9742aa45b2e49
Divya Manian <divya.manian@gmail.com>
2010-07-09 21:03:58 -0700

    adding mouseenter/leave

--- jquery.weemarquee.js
+++ jquery.weemarquee.js
@@ -5,12 +5,14 @@
             interval: 20, 
             fade: true, 
             width: 400, 
-            skin: 'wm-base'
+            skin: 'wm-base' 
         };
         
         if (settings) $.extend(config, settings);
-                                                
-        if(config.fade) {                       
+        
+                                               
+        if(config.fade) { 
+           // Add the necessary Classname for styling                      
           $(this).wrap("<div class='wm-fade " + config.skin + "'><div class='wm-mom'/></div>");  
           $(this).parent().prepend("<b/><i/>");
         } else {
@@ -20,26 +22,35 @@
         $(this).removeClass("wm-mom").addClass("marquee").parent().css({width : config.width});
         
         this.each(function() {              
-            var ticker = $(this);    
+            var ticker = $(this);        
+            
             var containerwidth = ticker.parent().outerWidth();                                
             var totalwidth = 0, leftvalue = 0, recursivemarquee;
             ticker.children().each(function() { 
-                $(this).addClass("wm-el")                
-                totalwidth += $(this).outerWidth(true);   
+                $(this).addClass("wm-el"); // As we dont know the kind of element that will be in it, we use this classname to style         
+                totalwidth += $(this).outerWidth(true);  
+                $(this).mouseenter(function(){
+                   clearInterval(interval);
+                });  
+                
+                $(this).mouseleave(function(){
+                  interval = setInterval(recursivemarquee, 30);
+                })
                
             });                                             
                                                
              
-            if(totalwidth > containerwidth) {
+            if(totalwidth > containerwidth) { 
+              // Decide if we need to duplicate content depending on whether the content is longer than the marquee viewport
               ticker.children().each(function() {
                   $(this).clone().appendTo(ticker);
               });   
               
-              ticker.width(3*totalwidth);  
+              ticker.width(2*totalwidth);  
               
               recursivemarquee = function() {
                   leftvalue = parseInt(ticker.css('left'))? parseInt($(ticker).css('left')) : 0;
-                  leftvalue = (leftvalue%totalwidth)-1;                                                                                     
+                  leftvalue = (leftvalue%totalwidth)-1;  // Neat way to count till the width of the content and reset it.                                                                                    
                   ticker.css({ left: leftvalue});  
 
               };
@@ -48,8 +59,8 @@
 
               ticker.css({left: containerwidth});
               recursivemarquee = function() {
-                leftvalue = parseInt(ticker.css('left'));   
-                ((leftvalue < 0) && (Math.abs(leftvalue) > totalwidth))? leftvalue = containerwidth: --leftvalue;
+                leftvalue = parseInt(ticker.css('left')); // Looks ugly when it starts from the middle. 
+                ((leftvalue < 0) && (Math.abs(leftvalue) > totalwidth))? leftvalue = containerwidth: --leftvalue; 
                 ticker.css({ left: leftvalue});   
               };
             }
